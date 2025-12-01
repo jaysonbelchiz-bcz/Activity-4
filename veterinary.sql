a351b80076c977fd02a972939c9ecd1f6a544d42
hannahloys <hannahloisj@gmail.com>
2025-11-30 21:13:48 -0600

    feat/insert-10-rows-animals

--- veterinary.sql
+++ veterinary.sql
@@ -71,3 +71,14 @@ INSERT INTO owners(ownerid, ofirstname, olastname, address, phone, email) VALUES
 (9, 'Mark', 'Berte', 'Garcia', '09178987419', 'babunism@gmail.com'),
 (10,'John', 'Aquino', 'Davao', '09179906950', 'plottwist2025@gmail.com');
 
+INSERT INTO animals(animalid, name, species, breed, dateofbirth, gender, color, ownerid) VALUES
+(1, 'Sparky', 'Dog', 'Shih tzsu', 2021-05-09, 'Male', 'Brown', 1),
+(2, 'Lowie', 'Cat', 'Siberian', 2025-03-15, 'Male', 'White', 2),
+(3, 'Kitty', 'Dog', 'Bulldog', 2022-06-06, 'Male', 'Brown', 3),
+(4, 'Nemo', 'Fish', 'Tilapia', 2025-10-29, 'Female', 'Blue', 4),
+(5, 'Hope', 'Hamster', 'Roborovski', 2024-07-24, 'Female', 'Gray', 5),
+(6, 'Prada', 'Cat', 'Burmese', 2023-05-19, 'Female', 'Brown', 6),
+(7, 'Sophie', 'Dog', 'Chihuahua', 2019-01-01, 'Female', 'White', 7),
+(8, 'Babu', 'Cat', 'Siberian', 2019-10-11, 'Female', 'White', 8),
+(9, 'Toni', 'Fish', 'Goldfish', 2024-12-25, 'Female', 'Gold', 9),
+(10, 'Onyx', 'Dog', 'Shih Tzu', 2020-10-13, 'Male', 'Black', 10);
