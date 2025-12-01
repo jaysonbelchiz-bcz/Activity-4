3a920e63916c4defe93930c059e5bd3c8b5929e6
jaysonbelchiz-bcz <jaysonbelchiz@gmail.com>
2025-12-01 11:02:16 +0800

    feat/insert-10-rows-owners

--- veterinary.sql
+++ veterinary.sql
@@ -45,3 +45,15 @@ CREATE TABLE invoices (
     paymenttime TIME,
     FOREIGN KEY (appointid) REFERENCES appointments(appointid)
 );
+
+INSERT INTO owners(ownerid, ofirstname, olastname, address, phone, email) VALUES
+(1, 'Jayson', 'Belchiz', 'Garcia', '09542567850', 'jayzbcz@gmail.com@gmail.com'),
+(2, 'Shiey', 'Cortez', 'San Jose', '09171176543', 'shiellah@gmail.com'),
+(3, 'Franchesca', 'Marasigan', 'Baguio', '09170922221', 'chescakulit81@gamil.com'),
+(4, 'Mae', 'Bet', 'Manila', '09167534444', 'maebetuwu076@gmail.com'),
+(5, 'Frances', 'Cute', 'Batangas', '09174445985', 'francesnism72@gmail.com'),
+(6, 'Bin', 'Albin', 'Quezon', '09175507636', 'binbin009@gmail.com'),
+(7, 'Christian', 'De Chavez', 'Kawit', '09176660177', 'chrstianmo069@gmail.com'),
+(8, 'Hannah', 'Berte', 'Rosario', '09177778888', 'asawanibabu@gmail.com'),
+(9, 'Mark', 'Berte', 'Garcia', '09178987419', 'babunism@gmail.com'),
+(10,'John', 'Aquino', 'Davao', '09179906950', 'plottwist2025@gmail.com');
